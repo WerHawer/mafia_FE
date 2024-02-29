b4c666c4d3f58254b0ada38f6336a77ba43d34a0
Konstantin Dmitrenko <werhawer@gmail.com>
2024-02-29 11:56:29 +0200

    add peer.secure

--- src/api/apiConstants.ts
+++ src/api/apiConstants.ts
@@ -3,7 +3,6 @@ import { projectEnv } from '../config/projectEnv';
 export const SERVER = projectEnv.apiUrl;
 export const PEER_SERVER = projectEnv.peerUrl;
 export const PEER_PORT = projectEnv.peerPort;
-export const PEER_SECURE = projectEnv.peerSecure;
 
 export const queryKeys = {
   users: 'users',
