cb9ad6fec7dfa70829b028558928f2c3fb8df911
Konstantin Dmitrenko <werhawer@gmail.com>
2025-09-20 01:30:40 +0300

    fixe build

--- src/pages/Game/GamePage.tsx
+++ src/pages/Game/GamePage.tsx
@@ -1,12 +1,9 @@
-import { LiveKitRoom } from "@livekit/components-react";
 import classNames from "classnames";
 import { observer } from "mobx-react-lite";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { LIVEKIT_SERVER } from "@/api/apiConstants.ts";
 import { useAddUserToGameMutation } from "@/api/game/queries.ts";
-import { useGetLiveKitTokenMutation } from "@/api/livekit/queries.ts";
 import { useGetUsersWithAddToStore } from "@/api/user/queries.ts";
 import { GameChat } from "@/components/GameChat";
 import { GameInfoSection } from "@/components/GameInfoSection";
@@ -14,18 +11,15 @@ import { GameVideoContainer } from "@/components/GameVideoContainer";
 import { GameVote } from "@/components/GameVote";
 import { LiveKitMafiaRoom } from "@/components/LiveKitMafiaRoom/LiveKitMafiaRoom.tsx";
 import { VideoConfig } from "@/components/VideoConfig";
-import { videoOptions } from "@/config/video.ts";
-import { useUserMediaStream } from "@/hooks/useUserMediaStream.ts";
 import { rootStore } from "@/store/rootStore.ts";
 
 import styles from "./GamePage.module.scss";
 
 const GamePage = observer(() => {
   const { id = "" } = useParams();
-  const { usersStore, gamesStore, streamsStore } = rootStore;
+  const { usersStore, gamesStore } = rootStore;
   const { myId } = usersStore;
   const { setActiveGame, activeGamePlayers } = gamesStore;
-  const { myStream } = streamsStore;
   const { mutate: addUserToGame } = useAddUserToGameMutation();
 
   useGetUsersWithAddToStore(activeGamePlayers);
@@ -37,7 +31,7 @@ const GamePage = observer(() => {
   }, [id, setActiveGame]);
 
   useEffect(() => {
-    if (!myId || !id || !myStream) return;
+    if (!myId || !id) return;
 
     const requestTimer = setTimeout(() => {
       addUserToGame({
@@ -47,7 +41,7 @@ const GamePage = observer(() => {
     }, 0);
 
     return () => clearTimeout(requestTimer);
-  }, [id, addUserToGame, myId, myStream]);
+  }, [id, addUserToGame, myId]);
 
   return (
     <div className={styles.pageContainer}>
